25a30e5d2e4ad58f1aa9b24e592765fc4313f07c
koi <funkiin.forever@gmail.com>
2025-02-17 12:35:50 -0500

    update goes crazy
    newgrounds achievements update + sfx

--- source/states/QuizState.hx
+++ source/states/QuizState.hx
@@ -13,6 +13,11 @@ import game.SpeechBubble;
 import flixel.FlxSprite;
 import flixel.util.FlxColor;
 import flixel.util.FlxTimer;
+import io.newgrounds.NG;
+import api.APIStuff;
+import api.Newgrounds;
+import game.AchievementNotif;
+import flixel.sound.FlxSound;
 
 class QuizState extends FlxState
 {
@@ -29,16 +34,97 @@ class QuizState extends FlxState
 	private var rightSpeechBubble:SpeechBubble;
 	private var wrongSpeechBubble:SpeechBubble;
 	
+	private var correctSound:FlxSound;
+	private var wrongSound:FlxSound;
+	
+	private var usernameText:FlxText;
+	private var medalText:FlxText;
+	
 	override public function create()
 	{
 		super.create();
+				
+		#if html5
+		correctSound = FlxG.sound.load("assets/sounds/correct.mp3");
+		wrongSound = FlxG.sound.load("assets/sounds/wrong.mp3");
+		#else
+		correctSound = FlxG.sound.load("assets/sounds/correct.ogg");
+		wrongSound = FlxG.sound.load("assets/sounds/wrong.ogg");
+		#end
 		
 		initializeQuestions();
 		setupUI();
 		showCurrentQuestion();
+
+		usernameText = new FlxText(FlxG.width - 200, 10, 190, "Connecting...", 16);
+		usernameText.alignment = RIGHT;
+		usernameText.color = FlxColor.WHITE;
+		usernameText.setBorderStyle(OUTLINE, FlxColor.BLACK, 2);
+		
+		medalText = new FlxText(FlxG.width - 200, 50, 190, "", 16);
+		medalText.alignment = RIGHT;
+		medalText.color = FlxColor.YELLOW;
+		medalText.setBorderStyle(OUTLINE, FlxColor.BLACK, 2);
+		
+		add(usernameText);
+		add(medalText);
 		
 		audience = new Audience();
 		add(audience);
+		
+		if (FlxG.save.data.unlockedMedals == null) {
+			FlxG.save.data.unlockedMedals = 0;
+			FlxG.save.flush();
+		}
+		
+		trace("Loaded medals: " + FlxG.save.data.unlockedMedals);
+		medalText.text = "Medals: " + FlxG.save.data.unlockedMedals + "/2";
+		
+		#if html5
+		try {
+			var ng:Newgrounds = new Newgrounds(APIStuff.API, APIStuff.EncKey, FlxG.save.data.sessionId);
+			trace('NEWGROUNDS LOL');
+
+			NG.core.onLogin.add(function() {
+				trace("Login event triggered");
+				if (NG.core != null && NG.core.user != null)
+				{
+					trace("User logged in! Username: " + NG.core.user.name);
+					usernameText.text = "Welcome, " + NG.core.user.name + "!";
+					medalText.text = "Medals: " + FlxG.save.data.unlockedMedals + "/2";
+					FlxG.save.data.sessionId = NG.core.sessionId;
+					FlxG.save.flush();
+				}
+			});
+
+			#if !debug
+			if (NG.core.user == null)
+			{
+				usernameText.text = "Loading...";
+				usernameText.color = FlxColor.WHITE;
+				
+				new FlxTimer().start(2.0, function(tmr:FlxTimer) {
+					if (NG.core.user == null)
+					{
+						usernameText.text = "Not logged in";
+					}
+				});
+			}
+			else
+			{
+				usernameText.text = "Welcome, " + NG.core.user.name + "!";
+				medalText.text = "Medals: " + FlxG.save.data.unlockedMedals + "/2";
+			}
+			#else
+			usernameText.text = "Debug Mode";
+			#end
+		} catch(e) {
+			trace("Newgrounds initialization failed: " + e);
+			usernameText.text = "Offline Mode";
+		}
+		#else
+		usernameText.text = "Desktop Build";
+		#end
 	}
 	
 	private function initializeQuestions()
@@ -108,7 +194,7 @@ class QuizState extends FlxState
 				["Chilidogs", "What??", "speed", "No"],
 				0,
 				[3], // Wrong Team
-				[0] // Right Team
+				[0, 1, 2] // Right Team
 			),
 			new Question(
 				"What is the capital of France?",
@@ -279,6 +365,28 @@ class QuizState extends FlxState
 		}
 		
 		#end
+
+		#if html5
+		if (FlxG.mouse.justPressed && usernameText.text == "Click here to login")
+		{
+			if (FlxG.mouse.overlaps(usernameText))
+			{
+				trace("Login text clicked, requesting login...");
+				NG.core.requestLogin(function() {
+					trace("Login callback triggered");
+					if (NG.core != null && NG.core.user != null)
+					{
+						Newgrounds.onNGLogin();
+						trace("Login successful! Username: " + NG.core.user.name);
+						usernameText.text = "Welcome, " + NG.core.user.name + "!";
+						medalText.text = "Medals: " + FlxG.save.data.unlockedMedals + "/2";
+						FlxG.save.data.sessionId = NG.core.sessionId;
+						FlxG.save.flush();
+					}
+				});
+			}
+		}
+		#end
 	}
 	
 	private function onAnswerClick(answerIndex:Int)
@@ -294,6 +402,7 @@ class QuizState extends FlxState
 			audience.celebrateRight();
 			rightSpeechBubble.playEmote("wrong_angry");
 			wrongSpeechBubble.playEmote("right_smiley");
+			wrongSound.play();
 		}
 		else if (question.isWrongTeamAnswer(answerIndex))
 		{
@@ -301,6 +410,7 @@ class QuizState extends FlxState
 			audience.celebrateWrong();
 			rightSpeechBubble.playEmote("wrong_smiley");
 			wrongSpeechBubble.playEmote("right_angry");
+			correctSound.play();
 		}
 		
 		currentQuestionIndex++;
@@ -333,12 +443,46 @@ class QuizState extends FlxState
 		
 		if (wasRightTeamDominant)
 		{
-			endingText.text = "You sided with the right team most of the time...\nbut at what cost?\nHalf of the audience leaves disappointed.\nBAD ENDING";
+			#if html5
+			Newgrounds.unlockMedal(83098);
+			#end
+			
+			if (FlxG.save.data.unlockedMedals < 1) {
+				FlxG.save.data.unlockedMedals++;
+				FlxG.save.flush();
+				trace("Saved medals: " + FlxG.save.data.unlockedMedals);
+				add(new AchievementNotif("Bad Ending?..."));
+			}
+			
+			#if html5
+			medalText.text = "Medals: " + FlxG.save.data.unlockedMedals + "/2";
+			#else
+			medalText.text = "Achievements: " + FlxG.save.data.unlockedMedals + "/2";
+			#end
+			
+			endingText.text = "You sided with the left team most of the time...\nbut at what cost?\nHalf of the audience leaves disappointed.\nBAD ENDING?...";
 			endingText.color = FlxColor.RED;
 		}
 		else
 		{
-			endingText.text = "You mostly listened to other half of the audience!\nSome people had fun and made new friends.\nGOOD ENDING";
+			#if html5
+			Newgrounds.unlockMedal(83097);
+			#end
+			
+			if (FlxG.save.data.unlockedMedals < 2) {
+				FlxG.save.data.unlockedMedals++;
+				FlxG.save.flush();
+				trace("Saved medals: " + FlxG.save.data.unlockedMedals);
+				add(new AchievementNotif("Good Ending?..."));
+			}
+			
+			#if html5
+			medalText.text = "Medals: " + FlxG.save.data.unlockedMedals + "/2";
+			#else
+			medalText.text = "Achievements: " + FlxG.save.data.unlockedMedals + "/2";
+			#end
+			
+			endingText.text = "You mostly listened to right team's answers!\nSome people had fun and made new friends.\nGOOD ENDING?...";
 			endingText.color = FlxColor.GREEN;
 		}
 		
